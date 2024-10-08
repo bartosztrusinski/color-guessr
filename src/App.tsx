9947b5e3bb092451503cee5738862c69dcbb315c
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-09 01:07:20 +0200

    config prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <main class='bg-slate-900 min-h-screen flex flex-col gap-4 items-center justify-center'>
-      <h1 class='text-4xl p-2 font-bold text-slate-200'>Hello Solid</h1>
-      <button class='btn btn-primary'>Nice button</button>
+    <main class="flex min-h-screen flex-col items-center justify-center gap-4 bg-slate-900">
+      <h1 class="p-2 text-4xl font-bold text-slate-200">Hello Solid</h1>
+      <button class="btn btn-primary">Nice button</button>
     </main>
   );
 };
