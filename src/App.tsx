52b61a5cdc39086fd835ad5942e9abb0c09b22f1
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-09 00:41:49 +0200

    install daisyui

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import type { Component } from 'solid-js';
 
 const App: Component = () => {
   return (
-    <main class='bg-slate-900 min-h-screen flex items-center justify-center'>
-      <h1 class='text-4xl p-2 font-bold text-slate-200'>Hello Solid</h1>;
+    <main class='bg-slate-900 min-h-screen flex flex-col gap-4 items-center justify-center'>
+      <h1 class='text-4xl p-2 font-bold text-slate-200'>Hello Solid</h1>
+      <button class='btn btn-primary'>Nice button</button>
     </main>
   );
 };
