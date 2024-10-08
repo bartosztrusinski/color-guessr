4dbfcd25bd951899f28023f6d4a51c7d5cd70af7
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-09 00:30:36 +0200

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
-  return <h1>Hello Solid</h1>;
+  return (
+    <main class='bg-slate-900 min-h-screen flex items-center justify-center'>
+      <h1 class='text-4xl p-2 font-bold text-slate-200'>Hello Solid</h1>;
+    </main>
+  );
 };
 
 export default App;
