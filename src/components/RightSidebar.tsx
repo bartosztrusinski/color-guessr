b0f2ba28d23573938e437eac95c1af5745d628e1
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-12 22:42:50 +0200

    create initial sidebar components

---
+++ src/components/RightSidebar.tsx
@@ -0,0 +1,7 @@
+import { ParentComponent } from 'solid-js';
+
+export const RightSidebar: ParentComponent = (props) => {
+  return (
+    <aside class="hidden flex-col gap-2 py-2 lg:flex">{props.children}</aside>
+  );
+};
