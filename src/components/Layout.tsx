9756b410f775d95db6f3dbe7f04488e5bdb369f9
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-12 22:42:33 +0200

    create initial responsive layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,8 @@ import { ParentComponent } from 'solid-js';
 
 export const Layout: ParentComponent = (props) => {
   return (
-    <div class="h-screen">
-      <main class="mx-auto h-full w-full max-w-prose p-4">
-        {props.children}
-      </main>
+    <div class="grid h-screen grid-cols-1 justify-center md:grid-cols-[var(--sidebar-col)_var(--game-col)] lg:grid-cols-[var(--sidebar-col)_var(--game-col)_var(--sidebar-col)]">
+      {props.children}
     </div>
   );
 };
