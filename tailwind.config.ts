cb5d3aa505ccf3d4a285d136907a132030334a50
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-13 23:45:34 +0200

    add display font and gradient to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,18 @@
-import type { Config } from 'tailwindcss'
-import daisyui from 'daisyui'
+import type { Config } from 'tailwindcss';
+import daisyui from 'daisyui';
 
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-colorful':
+          'linear-gradient(-20deg, #003f5b, #2b4b7d, #5f5195, #98509d, #cc4c91, #f25375, #ff6f4e, #ff9913);',
+      },
+      fontFamily: {
+        display: ['Righteous', 'sans-serif'],
+      },
+    },
   },
   plugins: [daisyui],
-} satisfies Config
+} satisfies Config;
