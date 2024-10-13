609116cd76f106b2f9969ad31d8b97910f9b82b3
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-13 23:49:03 +0200

    create logo icon component

---
+++ src/components/LogoIcon.tsx
@@ -0,0 +1,23 @@
+import { Component } from 'solid-js';
+
+type Props = {
+  class?: string;
+};
+
+export const LogoIcon: Component<Props> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class={`fill-none stroke-current stroke-2 ${props.class}`}
+    >
+      <line x1="6" x2="10" y1="12" y2="12" />
+      <line x1="8" x2="8" y1="10" y2="14" />
+      <line x1="15" x2="15.01" y1="13" y2="13" />
+      <line x1="18" x2="18.01" y1="11" y2="11" />
+      <rect width="20" height="12" x="2" y="6" rx="2" />
+    </svg>
+  );
+};
