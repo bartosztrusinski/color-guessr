f054a863de06fc0668632f65c8ba7eab604cf9ea
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-14 23:49:01 +0200

    use new components, add spacing and description styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,13 @@ import { Board } from './components/Board';
 import { Layout } from './components/Layout';
 import { LeftSidebar } from './components/LeftSidebar';
 import { RightSidebar } from './components/RightSidebar';
-import { LogoIcon } from './components/LogoIcon';
+import { Logo } from './components/Logo';
+import { DifficultySelect } from './components/DifficultySelect';
+import { MobileDrawer } from './components/MobileDrawer';
 
 import { Difficulty, GameState } from './types';
-import { defaultDifficulty, difficultySettings } from './config';
-import {
-  capitalize,
-  generateRandomColors,
-  getBoardSize,
-  getKeys,
-  pickRandomIndex,
-} from './utils';
+import { defaultDifficulty } from './config';
+import { generateRandomColors, getBoardSize, pickRandomIndex } from './utils';
 
 const App: Component = () => {
   const [gameState, setGameState] = createSignal<GameState>('playing');
@@ -57,36 +53,36 @@ const App: Component = () => {
   return (
     <Layout>
       <LeftSidebar>
-        <header class="px-4">
-          <div class="flex items-center justify-center gap-2 py-2">
-            <LogoIcon class="bg-gradient-colorful size-12 -rotate-12 rounded-lg p-2 text-slate-50" />
-            <h1 class="bg-gradient-colorful font-display bg-clip-text text-3xl font-bold text-transparent">
-              ColorGuessr
-            </h1>
+        <header class="mb-4 mt-3 px-4">
+          <Logo />
+          <div class="chat chat-end my-4 px-6">
+            <div class="chat-bubble chat-bubble-primary text-balance text-center text-slate-50">
+              Try and guess the color based on the RGB values! 🌈
+            </div>
           </div>
-          <p class="text-pretty py-1 text-lg">
-            Try and guess the color based on the RGB values
-          </p>
         </header>
-        <p class="mt-2 self-center">Difficulty: {difficulty()}</p>
-        <div class="mt-3 self-center">
-          <h2 class="mb-1 text-lg">Select difficulty</h2>
-          <div class="join">
-            {getKeys(difficultySettings).map((difficulty) => (
-              <button
-                type="button"
-                class="btn btn-primary join-item"
-                onClick={() => changeDifficulty(difficulty)}
-              >
-                {capitalize(difficulty)}
-              </button>
-            ))}
-          </div>
-        </div>
+        <DifficultySelect
+          currentDifficulty={difficulty()}
+          onClick={changeDifficulty}
+        />
       </LeftSidebar>
 
-      <main class="p-6 text-center md:px-4">
-        <p class="mb-4 text-3xl font-bold">
+      <main class="p-4 text-center md:py-8">
+        <MobileDrawer>
+          <header class="mb-4 mt-3">
+            <Logo />
+            <div class="chat chat-end my-5 px-4">
+              <div class="chat-bubble chat-bubble-primary text-balance text-center text-slate-50">
+                Try and guess the color based on the RGB values! 🌈
+              </div>
+            </div>
+          </header>
+          <DifficultySelect
+            currentDifficulty={difficulty()}
+            onClick={changeDifficulty}
+          />
+        </MobileDrawer>
+        <p class="pb-4 text-3xl font-bold">
           {gameState() === 'playing' ?
             <p>RGB ({Object.values(winningColor()).join(', ')})</p>
           : isWin() ?
@@ -96,7 +92,7 @@ const App: Component = () => {
         {!isPlaying() && (
           <button
             type="button"
-            class="btn btn-secondary mb-4"
+            class="btn btn-secondary btn-wide mb-4"
             onClick={initializeGame}
           >
             Play again
@@ -105,9 +101,7 @@ const App: Component = () => {
         <Board colors={colors()} onClick={guessColor} />
       </main>
 
-      <RightSidebar>
-        <span class="text-center text-4xl font-bold">🚧WIP🚧</span>
-      </RightSidebar>
+      <RightSidebar></RightSidebar>
     </Layout>
   );
 };
