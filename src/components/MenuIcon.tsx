6c2bd719790333fc36e3b050bf40f852a1f75051
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-10-14 23:41:32 +0200

    create menu icon component and add default size

--- src/components/MenuIcon.tsx
+++ src/components/MenuIcon.tsx
@@ -4,7 +4,7 @@ type Props = {
   class?: string;
 };
 
-export const LogoIcon: Component<Props> = (props) => {
+export const MenuIcon: Component<Props> = (props) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -13,11 +13,9 @@ export const LogoIcon: Component<Props> = (props) => {
       stroke-linejoin="round"
       class={`size-12 fill-none stroke-current stroke-2 ${props.class}`}
     >
-      <line x1="6" x2="10" y1="12" y2="12" />
-      <line x1="8" x2="8" y1="10" y2="14" />
-      <line x1="15" x2="15.01" y1="13" y2="13" />
-      <line x1="18" x2="18.01" y1="11" y2="11" />
-      <rect width="20" height="12" x="2" y="6" rx="2" />
+      <line x1="4" x2="20" y1="12" y2="12" />
+      <line x1="4" x2="20" y1="6" y2="6" />
+      <line x1="4" x2="20" y1="18" y2="18" />
     </svg>
   );
 };
