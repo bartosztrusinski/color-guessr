be1039c92049ba5eb93b8498cbe949d44a47a069
Bartosz Trusiński <bartosztrusinski@gmail.com>
2025-07-17 01:10:53 +0200

    show winning color on round results

--- src/components/round-results-modal.tsx
+++ src/components/round-results-modal.tsx
@@ -3,16 +3,26 @@ import { useAppContext } from '../context/app-context';
 import { Modal } from './modal';
 
 export function RoundResultsModal() {
-  const { appState, setAppState, isWin, startRound } = useAppContext();
+  const { appState, setAppState, isWin, startRound, winningColor } = useAppContext();
 
   return (
     <Modal isOpen={appState.isModalOpen} onClose={() => setAppState('isModalOpen', false)}>
-      <div class="flex flex-col items-center gap-7">
+      <div class="flex flex-col items-center gap-5">
         <p class="bg-gradient font-display bg-clip-text text-4xl font-bold text-transparent">
           <Show when={isWin()} fallback="You Lose">
             You Win
           </Show>
         </p>
+        <div
+          class="font-display bg-(--winning-color) bg-clip-text text-2xl font-medium text-transparent"
+          style={{
+            '--winning-color': `rgb(${winningColor().r}, ${winningColor().g}, ${winningColor().b})`,
+          }}
+        >
+          <div>RGB ({Object.values(winningColor()).join(', ')})</div>
+          <div class="mx-auto mt-1 size-16 rounded bg-inherit shadow-lg"></div>
+        </div>
+
         <p class="text-lg font-medium">
           <Switch fallback="Better luck next time!">
             <Match when={appState.isNewTopScore}>Wow! You just beat the top score!</Match>
